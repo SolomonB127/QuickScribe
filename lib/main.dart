483b993335bc5578669a0ac4593b83e954da14e2
SolomonB127 <solomonakinsulire@gmail.com>
2024-09-27 20:56:31 +0100

    Wrapped the MainApp method with the ChangeNotifierProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:quick_scribe/models/note_db.dart';
 
 import 'pages/notes_page.dart';
@@ -7,7 +8,8 @@ void main() async {
   // initialize note isar DB
   WidgetsFlutterBinding.ensureInitialized();
   await NotesDb.initialize();
-  runApp(const MainApp());
+
+  runApp(ChangeNotifierProvider(create: (context) => NotesDb(), child: const MainApp(),));
 }
 
 class MainApp extends StatelessWidget {
