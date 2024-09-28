601d0f9c31945d42b505be1b982605ed187bd7f9
SolomonB127 <solomonakinsulire@gmail.com>
2024-09-28 01:03:52 +0100

    feat:Added Create and Read methods - Created the method for creating a new note by clicking on a button - Created notes read method, and displayed created notes to UI

--- lib/pages/notes_page.dart
+++ lib/pages/notes_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:quick_scribe/models/note_db.dart';
+import 'package:quick_scribe/models/notes.dart';
 
 class NotesPage extends StatefulWidget {
   const NotesPage({super.key});
@@ -8,8 +11,66 @@ class NotesPage extends StatefulWidget {
 }
 
 class _NotesPageState extends State<NotesPage> {
+  // text controller for tracking text strokes
+  final _createController = TextEditingController();
+  // create a note
+  void createNote() {
+    showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+              content: TextField(
+                controller: _createController,
+              ),
+              actions: [
+                // create note
+                MaterialButton(
+                  onPressed: () {
+                    // add to db
+                    context.read<NotesDb>().addNote(_createController.text);
+                    // pop dialog box
+                    Navigator.pop(context);
+                  },
+                  child: const Text("Ok"),
+                )
+              ],
+            ));
+  }
+
+  // read a note
+  void readNotes() {
+    context.watch<NotesDb>().fetchNotes();
+  }
+
+  // update a note
+
+  // delete a note
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    // notes DB
+    final notesdb = context.watch<NotesDb>();
+
+    // currents note
+    List<Note> currentNote = notesdb.currentNotes;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Notes"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => createNote,
+        child: const Icon(Icons.add),
+      ),
+      body: ListView.builder(
+        itemCount: currentNote.length,
+        itemBuilder: (context, index) {
+          // individual notes
+          final note = currentNote[index];
+
+          // List Ui
+          return ListTile(
+            title: Text(note.text),
+          );
+        },
+      ),
+    );
   }
 }
