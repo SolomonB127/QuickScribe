9128415f61fc94cee1cc368a5a51fa67257deb32
SolomonB127 <solomonakinsulire@gmail.com>
2024-09-28 01:53:41 +0100

    feat: Created a themes directory - contains a theme.dart file - Theme.dart contains color schemes for light and dark modes

---
+++ lib/theme/theme.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+// lightmode
+ThemeData lightmode = ThemeData(
+    brightness: Brightness.light,
+    colorScheme: ColorScheme.light(
+        surface: Colors.grey.shade300,
+        primary: Colors.grey.shade200,
+        secondary: Colors.grey.shade400,
+        inversePrimary: Colors.grey.shade800));
+
+// darkmode
+ThemeData darkmode = ThemeData(
+    brightness: Brightness.dark,
+    colorScheme: ColorScheme.dark(
+        surface: Colors.grey.shade900,
+        primary: Colors.grey.shade800,
+        secondary: Colors.grey.shade700,
+        inversePrimary: Colors.grey.shade300));
